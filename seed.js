60765d10cfc25d7891a1f06cd8c66ff14f9afc95
张涛 <litson.zhang@gmail.com>
2015-04-01 11:10:05 +0800

    updates

--- seed.js
+++ seed.js
@@ -1,5 +1,13 @@
 /**
- * [__seed_package__ description]
+ *
+ * TODO:
+ *     1.支持JSONP拉取combo数据
+ *     2.若拉取js出现异常，采用创建script + src的传统方式下载 (会影响执行速度)
+ *     3.本地combo文件
+ *     4.CSS文件并发下载
+ *     5.expires 可否 改为 version方式
+ *     6.deal buttom top
+ * 
  * @param  {[type]} root      [description]
  * @param  {[type]} undefined [description]
  * @return {[type]}           [description]
@@ -14,24 +22,29 @@
 
     var ls = root.localStorage;
     var doc = document;
-    var header = doc.head;
-    var body = doc.body;
+    // var header = doc.head;
+    // var body = doc.body;
 
     var ABSOLUTE_RE = /^\/\/.|:\//;
     var IS_CSS_RE = /\.css(?:\?|$)/i;
+    var PARAM_RE = /^(.*\.(?:css|js))(.*)$/i;
+
+    // console.dir('http://dfa.cn/b.js?abc'.match(PARAM_RE));
 
     var seed = root.Seed = {
-        DEBUG: true,
         // getFile: getFile,
         support: isSupportLocalStorage(),
         use: use,
         config: config,
         openRealtimeDebugMode: openRealtimeDebugMode,
-        // cache: {}
+        scan: scan,
+        cache: {},
         version: '1.0.0'
     };
 
-    var data = seed.data = {};
+    var data = seed.data = {
+        debug: false
+    };
 
     data.base = location.origin;
 
@@ -47,27 +60,29 @@
             if (getType(ids) === 'string') {
                 ids = [ids];
             } else {
-                log('warn', '[类型错误]请提供一个文件名或者文件名数组！');
+                log('warn', '{fn:Seed.use} --- ', '[类型错误]请提供一个文件名或者文件名数组！');
                 return false;
             }
         };
 
-        log('原始数据：', ids);
+        log('{fn:Seed.use} --- ', '原始数据：', ids);
 
         ids = parseIds(ids);
 
-        log('路由寻找到：', ids);
+        log('{fn:Seed.use} --- ', '路由寻找到：', ids);
+
 
         ids = ids.filter(function(item) {
-            if (!doc.getElementById(item)) {
+            if (!seed.cache[item.id]) {
                 return item;
             } else {
-                log('warn', item, '已经被下载！');
+                log('warn', '{fn:Seed.use} --- ', item.id, '已被下载！');
             }
         });
 
         ids.length && dock(ids, function(codeStringQueue) {
-            codeStringQueue.forEach(function(item, index) {
+            codeStringQueue.forEach(function(item) {
+                seed.cache[item.id] = !0;
                 executeCode(item.data, item.id);
             });
             callBack && callBack();
@@ -111,6 +126,34 @@
         return seed;
     };
 
+
+    /**
+     * 未完成
+     * @return {[type]} [description]
+     */
+    function scan() {
+        var files = doc.querySelectorAll('[data-seed]');
+
+        var len = files.length;
+        if (!len) {
+            return seed;
+        }
+
+        var ids = [];
+        var i = 0;
+        var temp;
+
+        for (; i < len; i++) {
+            temp = files[i];
+            ids.push(temp.dataset.seed);
+            temp.parentNode.removeChild(temp);
+        }
+
+        log('{fn:Seed.scan} --- ', '获取到的种子：', ids);
+
+        return seed.use(ids);
+    };
+
     /**
      * [parseAlias description]
      * @param  {[type]} id [description]
@@ -122,14 +165,57 @@
     };
 
     /**
-     * [parseMap description]
+     *
+     * 'http://example.org/exp.js?hook' ==> 
+     * {
+     *     id:   'http://example.org/exp.js',
+     *     hook: '?hook'
+     * }
+     * 
+     * 
      * @param  {[type]} id [description]
      * @return {[type]}    [description]
      */
-    function parseMap(id) {
+    function parseHook(originalId) {
+
         var map = data.map;
-        var result = id;
-        return result;
+
+        var splited;
+        var id;
+
+        var hook = '__seed_version__';
+        var mapType = getType(map);
+
+        // 如果映射规则是函数
+        if (mapType === 'function') {
+
+            splited = map(originalId);
+            // 如果映射规则是正则
+        } else if (mapType === 'regexp') {
+
+            splited = originalId.match(map);
+            // 如果是错误的映射规则
+        } else {
+            splited = originalId.match(PARAM_RE);
+        }
+
+        // 当映射规则出现故障时，可能会得不到预期的结果
+        if (getType(splited) !== 'array') {
+            log('warn', '{fn:parseHook} --- ', '\n解析映射规则失败 by\n', map, '\n', originalId, '\n', '使用默认规则');
+            splited = originalId.match(PARAM_RE);
+        }
+
+        id = splited[1];
+
+        // 若解析后，id未发生变化，则使用默认的hook规则
+        if (id !== originalId) {
+            hook = splited[2];
+        }
+
+        return {
+            id: id,
+            hook: hook
+        };
     };
 
     /**
@@ -139,7 +225,7 @@
      */
     function parseIds(ids) {
         var result = [];
-        ids.forEach(function(item, index) {
+        ids.forEach(function(item) {
 
             /**
              * 104 === h ( http, https )
@@ -147,23 +233,18 @@
              * 47  === / ( / )
              */
             // var charCode = item.charCodeAt(0);
-
             var item = item.trim();
-
             if (!ABSOLUTE_RE.test(item)) {
                 // 如果非绝对路径，则先在别名中查找
                 var relative = parseAlias(item);
                 // 如果别名中已存在该路径，则使用
                 item = relative ? relative : item;
-                // 再做一次十分是绝对路径的检测，因为允许在别名中配置其他‘绝对路径的文件’；
+                // 再做一次绝对路径的检测，因为允许在alias中配置其他‘绝对路径的文件’；
                 item = (ABSOLUTE_RE.test(item) ? item : data.base + item);
                 // TODO:匹配映射，去掉版本号什么的。
-                item = parseMap(item);
+                item = parseHook(item);
             }
-
             result.push(item);
-
-
         });
         return result;
     };
@@ -183,8 +264,8 @@
 
         // 判断是不是invlid date,列举了3中方法。。
         // expire = (expire.getDate().toString() === 'NaN') ? 0 : (+expire);
-        // expire = (Date.prototype.toString.call(expire) === 'Invalid Date') ? 0 : (+expire);
-        expire = (expire.valueOf().toString() === 'NaN') ? 0 : (+expire);
+        expire = (Date.prototype.toString.call(expire) === 'Invalid Date') ? 0 : (+expire);
+        // expire = (expire.valueOf().toString() === 'NaN') ? 0 : (+expire);
 
         // 用户设置了过期时间，则对比是否已过期
         return ms < data.expires;
@@ -206,15 +287,23 @@
         var target;
 
         isCSS
-            ? (fileType = 'style', fileWayValue = 'text/css', target = header) : (fileType = 'script', fileWayValue = 'text/javascript', target = body);
+            ? (fileType = 'style', fileWayValue = 'text/css', target = doc.header) : (fileType = 'script', fileWayValue = 'text/javascript', target = doc.body);
 
         node = doc.createElement(fileType);
         node.id = uid;
         node.type = fileWayValue;
         target.appendChild(node);
-        node.innerHTML = codeString;
-        log('已执行：', uid);
+        // node.innerHTML = codeString;
+        // innerHTML再js文件超过大概5000行的时候会出输入超限异常。
+        node.appendChild(doc.createTextNode(codeString));
+        log('{fn:executeCode} --- ', '已执行：', uid);
     };
+
+
+    // function _exeScript
+
+
+
 
     /**
      * 根据一个ID的列表，下载
@@ -226,14 +315,17 @@
         var codeStringQueue = [];
         var fns = [];
 
-        var fnWrapperFromRemote = function(id) {
+        // 远程拉数据方法
+        var fnWrapperFromRemote = function(item) {
             return function(next) {
-                getFile(id, function(codeString) {
+                getFile(item.id, function(codeString) {
                     codeStringQueue.push({
                         data: codeString,
-                        id: id
+                        id: item.id
                     });
-                    seed.support && ls.setItem(id, codeString);
+
+                    seed.support && (ls.setItem(item.id, codeString), ls.setItem(item.id + '@hook', item.hook));
+
                     next();
                 }, function() {
                     next();
@@ -241,11 +333,12 @@
             }
         };
 
-        var fnWrapperFromLocal = function(id) {
+        // 本地查找
+        var fnWrapperFromLocal = function(item) {
             return function(next) {
                 codeStringQueue.push({
-                    data: ls.getItem(id),
-                    id: id
+                    data: ls.getItem(item.id),
+                    id: item.id
                 });
                 next();
             }
@@ -271,20 +364,26 @@
             return false;
         })();
 
-
         // 打包成一个函数队列
-        ids.forEach(function(id) {
+        ids.forEach(function(item) {
 
-            if (needLoadData) {
-                fns.push(fnWrapperFromRemote(id));
-            } else {
-                if (seed.support && !ls.getItem(id)) {
-                    fns.push(fnWrapperFromRemote(id));
-                } else {
-                    fns.push(fnWrapperFromLocal(id));
+            var condition = needLoadData || (function(item) {
+                // 不支持LS采用
+                if (!seed.support) {
+                    return true;
                 }
-            }
+                // 支持ls，但是本地中没有该item，或存在该item，但是版本号已经更新
+                if (!ls.getItem(item.id) || (ls.getItem(item.id + '@hook') !== item.hook)) {
+                    return true;
+                }
+                return false;
+            })(item);
 
+            log('{fn:dock} --- ', (condition ? '' : '不') + '需要下载：', item.id);
+
+            fns.push(
+                condition ? fnWrapperFromRemote(item) : fnWrapperFromLocal(item)
+            );
         });
 
         // 函数队列追加一个函数，待完成后执行回调
@@ -326,11 +425,12 @@
     function isSupportLocalStorage() {
         var support = true;
         try {
-            ls.setItem('_seed_test_', 1);
-            ls.removeItem('_seed_test_');
+            ls.setItem('__seed_test__', 1);
+            ls.removeItem('__seed_test__');
         } catch (e) {
             support = false;
         }
+        // log('{fn:isSupportLocalStorage} --- ', (support ? '' : '不'), 'localStorage');
         return support;
     };
 
@@ -341,14 +441,14 @@
      * @return {[type]}          [description]
      */
     function getFile(url, success, error) {
-        log('正在通过AJAX加载：', url);
+        log('{fn:getFile} --- ', '正在通过AJAX加载：', url);
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {
                 xhr.onreadystatechange = noop;
                 if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
-                    log('已加载：', url);
+                    log('{fn:getFile} --- ', '已加载：', url);
                     success && success(xhr.responseText, xhr);
                 } else {
                     getFileError(xhr.statusText || null, xhr.status ? 'error' : 'abort', xhr, error);
@@ -367,7 +467,7 @@
      * @return {[type]}       [description]
      */
     function getFileError(error, type, xhr, callBack) {
-        log('warn', '资源加载失败！', arguments);
+        log('warn', '{fn:getFileError} --- ', '资源加载失败！', arguments);
         callBack && callBack(error, type, xhr);
     };
 
@@ -378,10 +478,10 @@
     function log( /* type [, arg1, arg2...etc. ]*/ ) {
         var args = Array.prototype.slice.call(arguments);
         var type = args[0];
-        var isSpecified = !!~['warn', 'info', 'error', 'group', 'groupEnd'].indexOf(type);
+        var isSpecified = !!~['dir', 'warn', 'info', 'error', 'group', 'groupEnd'].indexOf(type);
         args = isSpecified ? (args.shift(), args) : args;
-        args.unshift('[Debug:]');
-        return seed.DEBUG && console[isSpecified ? type : 'log'].apply(console, args);
+        args.unshift('[Debug:] === ');
+        return data.debug && console[isSpecified ? type : 'log'].apply(console, args);
     };
 
 }(window);
